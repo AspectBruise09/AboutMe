5c5166c4a7a0c2c16e9db34ac245330f16aa9fe2
Artin <H.AspectBruise09@gmail.com>
2023-08-16 14:18:56 +0330

    Add files via upload

---
+++ AboutMe.py
@@ -0,0 +1,11 @@
+Life = "Normal"
+# Life = input("Enter Your Mental Status(Bad, Normal, Good): --> ")
+
+if Life == "Good":
+    print("Thanks God, For Everything.")
+elif Life == "Normal":
+    print("Just Right.")
+elif Life == "Bad":
+    print("FUCK MY LIFE!")
+else:
+    print("Life(Mental Status) Should Be Good, Normal, Bad")
