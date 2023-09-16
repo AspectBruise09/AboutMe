5037e24b60447f5079b17f8caacf94e0033c8646
Artin <H.AspectBruise09@gmail.com>
2023-09-16 19:35:44 +0330

    Update Oh-My-Posh Bash Install.sh

--- Oh-My-Posh Bash Install.sh
+++ Oh-My-Posh Bash Install.sh
@@ -1,1 +1,8 @@
+curl -s https://ohmyposh.dev/install.sh | bash -s
+wget -P $HOME https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json
+if [ -f ~/.bashrc ]; then
+    echo 'eval "$(oh-my-posh init bash --config ~/1_shell.omp.json)"' >> ~/.bashrc
+else
+    echo 'eval "$(oh-my-posh init bash --config ~/1_shell.omp.json)"' > ~/.bashrc
+fi
 
