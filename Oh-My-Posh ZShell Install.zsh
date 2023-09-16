72fd9b678552ba5bb7950c3102e9460774373ebb
Artin <H.AspectBruise09@gmail.com>
2023-09-16 21:20:35 +0330

    Update Oh-My-Posh ZShell Install.zsh

--- Oh-My-Posh ZShell Install.zsh
+++ Oh-My-Posh ZShell Install.zsh
@@ -1,1 +1,9 @@
-
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+brew update && brew upgrade oh-my-posh
+wget -P $HOME https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json
+if [ -f ~/.zshrc ]; then
+    echo 'eval "$(oh-my-posh init zsh --config ~/1_shell.omp.json)"' >> ~/.zshrc
+else
+    echo 'eval "$(oh-my-posh init zsh --config ~/1_shell.omp.json)"' > ~/.zshrc
+fi
+exec zsh
